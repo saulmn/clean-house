4c850d682c46bc9f655445458ff536fa8a5f811b
saulmn <saulmn.2599@gmai.com>
2025-09-29 20:38:33 -0600

    chore: Add future flags and rename main landing page folder

--- app/routes/_landing/_layout.tsx
+++ app/routes/_landing/_layout.tsx
@@ -1,8 +1,5 @@
 import { Outlet } from "@remix-run/react";
 
 export default function _layout() {
-  return (
-    
-      <Outlet />
-  );
+  return (<Outlet />);
 }
--- remix.config.js
+++ remix.config.js
@@ -2,16 +2,20 @@
 const { flatRoutes } = require("remix-flat-routes");
 
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
-  serverModuleFormat: "esm",
-  serverBuildPath: "build/index.mjs",
-  future:{
-    v2_dev: true,
-  },
-  routes: async (defineRoutes) => {
-    return flatRoutes("routes", defineRoutes, {
-      ignoredRouteFiles: ["**/index.ts"],
-    });
-  },
+	cacheDirectory: "./node_modules/.cache/remix",
+	ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+	serverModuleFormat: "esm",
+	serverBuildPath: "build/index.mjs",
+	future: {
+		v3_fetcherPersist: true,
+		v3_lazyRouteDiscovery: true,
+		v3_relativeSplatPath: true,
+		v3_singleFetch: true,
+		v3_throwAbortReason: true,
+	},
+	routes: async (defineRoutes) => {
+		return flatRoutes("routes", defineRoutes, {
+			ignoredRouteFiles: ["**/index.ts"],
+		});
+	},
 };
